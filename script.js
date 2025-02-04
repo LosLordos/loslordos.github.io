06285e2a8b97abde9d2e4d4317440b2d7213b6eb
loslordos <filipnekvasil15@seznam.cz>
2025-02-04 08:03:42 +0100

    import

---
+++ script.js
@@ -0,0 +1,21 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Close modal when clicking outside the modal content
+window.onclick = function (event) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+};
+
+
+
+
